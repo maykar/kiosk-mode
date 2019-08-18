2e326b17ada1f36f5d9b2cb33859ae3e368d80ad
Tero Mononen <tero@mononen.eu>
2019-08-18 23:02:47 +0300

    Fix compatibility with HASS 0.97.2; drop show_tabs.

--- kiosk.js
+++ kiosk.js
@@ -1,26 +1,29 @@
-if (window.location.href.indexOf('kiosk') > 0)
+if (window.location.href.indexOf('kiosk') > 0) {
     setTimeout(function () {
         try {
-            const root = document.querySelector('home-assistant').shadowRoot;
-            const main = root.querySelector('home-assistant-main').shadowRoot;
-            const drawer_layout = main.querySelector('app-drawer-layout');
-            const drawer = drawer_layout.querySelector('app-drawer');
-            drawer_sidebar = drawer.querySelector('ha-sidebar').shadowRoot;
-            const app_toolbar = drawer_sidebar.querySelector('app-toolbar');
-            const pages = drawer_layout.querySelector('partial-panel-resolver').shadowRoot;
-            const lovelace = pages.querySelector('ha-panel-lovelace').shadowRoot;
-            const huiroot = lovelace.querySelector('hui-root').shadowRoot;
-            const header = huiroot.querySelector('app-header');
-            const toolbar = huiroot.querySelector('app-toolbar');
-            if (window.location.href.indexOf('show_tabs') > 0) {
-                toolbar.style.display = 'none';
-            } else {
-                header.style.display = 'none';
-            }
-            app_toolbar.querySelector('paper-icon-button').click();
+            const home_assistant_main =  document
+                  .querySelector("body > home-assistant").shadowRoot
+                  .querySelector("home-assistant-main");
+            
+            const header = home_assistant_main.shadowRoot
+                  .querySelector("app-drawer-layout > partial-panel-resolver > ha-panel-lovelace").shadowRoot
+                  .querySelector("hui-root").shadowRoot
+                  .querySelector("#layout > app-header")
+                  .style.display = "none";
+            
+            const drawer = home_assistant_main.shadowRoot
+                  .querySelector("#drawer")
+                  .style.display = 'none';
+            
+            home_assistant_main.style.setProperty("--app-drawer-width", 0);
+            home_assistant_main.shadowRoot
+                .querySelector("#drawer > ha-sidebar").shadowRoot
+                .querySelector("div.menu > paper-icon-button")
+                .click();
             window.dispatchEvent(new Event('resize'));
         }
         catch (e) {
             console.log(e);
         }
     }, 200);
+}
