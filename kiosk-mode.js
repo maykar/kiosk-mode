21715236c7b433113808e94117709f6df3ed10b5
Ryan Meek <25127328+maykar@users.noreply.github.com>
2020-11-06 13:33:04 -0500

    More console tag tweaks

--- kiosk-mode.js
+++ kiosk-mode.js
@@ -126,14 +126,14 @@ function appLayoutWatch(mutations) {
 }
 
 // Overly complicated console tag.
-const conInfo = { header: "%c☰ KIOSK-MODE".padEnd(23), ver: "%cVersion: *DEV  " };
+const conInfo = { header: "%c≡ KIOSK-MODE".padEnd(23), ver: "%cVersion: *DEV " };
 const br = "%c\n";
 const maxLen = Math.max(...Object.values(conInfo).map((el) => el.length));
 for (const [key] of Object.entries(conInfo)) {
   if (conInfo[key].length <= maxLen) conInfo[key] = conInfo[key].padEnd(maxLen);
-  if (key == "header") conInfo[key] = `${conInfo[key].slice(0, -2)}⋮ `;
+  if (key == "header") conInfo[key] = `${conInfo[key]}⋮`;
 }
 const header =
   "display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;";
-const info = "border-width:0px 1px 1px 1px;padding:6px;background:white;color:#424242;line-height:0.7;";
+const info = "border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";
 console.info(conInfo.header + br + conInfo.ver, header, "", `${header} ${info}`);
