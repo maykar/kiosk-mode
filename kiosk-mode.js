25978cd15b9ea98ccd3dc31ef16e5292f8a290e9
Ryan Meek <25127328+maykar@users.noreply.github.com>
2021-01-12 18:10:25 -0500

    Refactor mobileWidth, move it up in order, add ignore_mobile_settings

--- kiosk-mode.js
+++ kiosk-mode.js
@@ -87,6 +87,7 @@ function kioskMode(lovelace, config, dash) {
   const userConfig = config.user_settings;
   const mobileConfig = config.mobile_settings;
   let ignoreEntity = false;
+  let ignoreMobile = false;
 
   // Retrieve localStorage values & query string options.
   let hideHeader = cached("kmHeader") || queryString(["kiosk", "hide_header"]);
@@ -102,6 +103,7 @@ function kioskMode(lovelace, config, dash) {
     hideHeader = adminConfig.kiosk || adminConfig.hide_header;
     hideSidebar = adminConfig.kiosk || adminConfig.hide_sidebar;
     ignoreEntity = adminConfig.ignore_entity_settings;
+    ignoreMobile = adminConfig.ignore_mobile_settings;
   }
 
   // Non-Admin user settings.
@@ -109,17 +111,7 @@ function kioskMode(lovelace, config, dash) {
     hideHeader = nonAdminConfig.kiosk || nonAdminConfig.hide_header;
     hideSidebar = nonAdminConfig.kiosk || nonAdminConfig.hide_sidebar;
     ignoreEntity = nonAdminConfig.ignore_entity_settings;
-  }
-
-  // Mobile settings.
-  if (mobileConfig) {
-    let mobile_width = 812;
-    if ("custom_width" in mobileConfig) mobile_width = mobileConfig.custom_width;
-    if (window.innerWidth <= mobile_width) {
-        hideHeader = mobileConfig.kiosk || mobileConfig.hide_header;
-      hideSidebar = mobileConfig.kiosk || mobileConfig.hide_sidebar;
-      ignoreEntity = mobileConfig.ignore_entity_settings;
-    }
+    ignoreMobile = nonAdminConfig.ignore_mobile_settings;
   }
 
   // User Settings.
@@ -129,7 +121,18 @@ function kioskMode(lovelace, config, dash) {
         hideHeader = conf.kiosk || conf.hide_header;
         hideSidebar = conf.kiosk || conf.hide_sidebar;
         ignoreEntity = conf.ignore_entity_settings;
+        ignoreMobile = conf.ignore_mobile_settings;
       }
+    }
+  }
+
+  // Mobile settings.
+  if (mobileConfig && !ignoreMobile) {
+    const mobileWidth = "custom_width" in mobileConfig ? mobileConfig.custom_width : 812;
+    if (window.innerWidth <= mobileWidth) {
+      hideHeader = mobileConfig.kiosk || mobileConfig.hide_header;
+      hideSidebar = mobileConfig.kiosk || mobileConfig.hide_sidebar;
+      ignoreEntity = mobileConfig.ignore_entity_settings;
     }
   }
 
